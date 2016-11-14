d01039fefbc4f55c31fac6ee0da26aad235a4319
Vagner Clementino <vagner.clementino@gmail.com>
2016-11-14 09:46:15 -0200

    Envio do questionário para o grupo python e stackoverflow

--- email_sender.py
+++ email_sender.py
@@ -2,48 +2,178 @@
 # -*- coding: utf-8 -*-
 import emails
 import docs.conf as cfg
-import datetime
+import argparse
+# import ipdb as pdb
+import csv
+import os.path
+from emails.template import JinjaTemplate as T
+import time
+import logging as log
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def parse_args():
+    """
+    Parse script input arguments.
+
+    Returns the parsed args, having validated that the input
+    file can be read, and that there is a valid Username.
+    """
+    parser = get_parser()
+    args = parser.parse_args()
+
+    # pdb.set_trace()
+    if os.path.isfile(args.html_filename):
+        args.html = args.html_filename
+    if os.path.isfile(args.recipients):
+        args.recipients_list = args.recipients
+    # print_args(args)
+    return args
+
+
+def get_parser():
+    """ Return the parser used to interpret the script arguments."""
+    usage = (
+        "Script to send an HTML file as an HTML email."
+        "\nExamples:"
+        "\n1. Send the contents of test_file.html to fred"
+        "\n$ send_html_email.py fred@example.com test_file.html"
+        "\n"
+       )
+    epilog = "NB This script requires a Gmail account."
+
+    formatter_class = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(description=usage,
+                                     epilog=epilog,
+                                     formatter_class=formatter_class
+                                     )
+    parser.add_argument('html_filename',
+                        help='The HTML file as Jinja template')
+    parser.add_argument('-r', '--recipients',
+                        help='A list on cvs format of the recipients'
+                        )
+    return parser
+
+
+def print_args(args):
+    """Print out the input arguments."""
+    print ('Sending test email by file: %s' % args.html)
+    print ('Sending teste email from list: %s' % args.recipients)
+
+
+def get_recipients(csv_file_path, limit=-1):
+    """TODO: Docstring for get_recipients.
+
+    :csv_file: TODO
+    :limit: TODO
+    :returns: TODO
+
+    """
+    # pdb.set_trace()
+    with open(csv_file_path, 'rb') as csv_file:
+        counter = 0
+        pythonbt_reader = csv.reader(csv_file,
+                                     delimiter=','
+                                     )
+        next(pythonbt_reader)
+        for row in pythonbt_reader:
+            if counter == limit:
+                return
+            else:
+                counter = counter + 1
+                yield row
 
 
 def main():
-    """TODO: Docstring for main.
-    :returns: None
+    """
+        TODO: Docstring for main.
+        :returns: None
 
     """
-    try:
-        retry = True
-        now = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        html_content = '<p><b>EU TE AMO! Mesmo as #h!</b></p>'
-        html_content = html_content.replace('#h', now, 1)
-        email_subejct = 'Amor da minha vida'
-        email_sender = ('Vagner Clementino',
-                        'vagnercs@dcc.ufmg.br')
-        email_receiver = ('Vagner Clementino',
-                          'vagnerclementino@pbh.gov.br')
-        email_cc = ("Andreza Vieira",
-                    'a.vieiralelis@gmail.com')
-        while retry:
-            message = emails.html(html=html_content,
-                                  subject=email_subejct,
-                                  mail_from=email_sender,
-                                  cc=email_cc
-                                  )
-            response = message.send(to=email_receiver,
-                                    smtp={'host': cfg.smtp_dcc['host'],
-                                          'port': cfg.smtp_dcc['port'],
-                                          'tls': cfg.smtp_dcc['tls'],
-                                          'user': cfg.smtp_dcc['user'],
-                                          'password': cfg.smtp_dcc['password']
-                                          }
-                                    )
-            if response.status_code not in [250, ]:
-                retry = True
-            else:
-                retry = False
-                message_text = message.as_string()
-                print("Enviado o e-mail:\n {0}".format(message_text))
-    except emails.backend.smtp.exceptions.SMTPConnectNetworkError as e:
-        print(e)
+    LIMIT_SENDER = 20
+    SECONDS_NEW_SEND = 60
+    log_path = './log/'
+    file_name = 'email_sender'
+    log_format = "[%(asctime)s] [%(levelname)s] : %(message)s"
+    logFormatter = log.Formatter(log_format,
+                                 datefmt='%d-%m-%Y %H:%m:%S'
+                                 )
+    rootLogger = log.getLogger()
+    rootLogger.setLevel(log.INFO)
 
+    fileHandler = log.FileHandler("{0}/{1}.log".format(log_path, file_name))
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+
+    consoleHandler = log.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+    args = parse_args()
+    total_email_enviados = 0
+
+    try:
+        log.info("Iniciando o processo de envio de email's")
+        retry = True
+        recipients_list = get_recipients(csv_file_path=args.recipients,
+                                         limit=LIMIT_SENDER)
+        with open(args.html, 'rb') as html_file:
+            html_content = T(html_file.read())
+            email_subejct = 'Survey about Issue Tracking System'
+            email_sender = ('Vagner Clementino',
+                            'vagnercs@dcc.ufmg.br')
+            for r in recipients_list:
+                real_name = unicode(r[0]).encode('utf8')
+                user_mail = r[1]
+                project_name = r[2]
+                email_ccb = ("Vagner Clementino",
+                             'vagner.clementino@gmail.com')
+                retry = True
+                while retry:
+                    message = emails.html(html=html_content,
+                                          subject=email_subejct,
+                                          mail_from=email_sender,
+                                          bcc=email_ccb
+                                          )
+                    smtp = {'host': cfg.smtp_dcc['host'],
+                            'port': cfg.smtp_dcc['port'],
+                            'tls': cfg.smtp_dcc['tls'],
+                            'user': cfg.smtp_dcc['user'],
+                            'password': cfg.smtp_dcc['password']
+                            }
+
+                    response = message.send(to=user_mail,
+                                            render={'real_name': real_name},
+                                            smtp=smtp
+                                            )
+                    if response.status_code not in [250, ]:
+                        retry = True
+                    else:
+                        retry = False
+                        log.info(("Enviado para o participante"
+                                  " {0} do Projeto {1}"
+                                  " através do e-mail {2}"
+                                  ).format(real_name,
+                                           project_name,
+                                           user_mail))
+                    u_message = ("Aguardando {0} segundos para um "
+                                 "novo envio").format(SECONDS_NEW_SEND)
+                    log.info(u_message)
+                    total_email_enviados = total_email_enviados + 1
+                    time.sleep(60)
+    except emails.backend.smtp.exceptions.SMTPConnectNetworkError as esmtp:
+        log.error(esmtp)
+    except IOError as ioe:
+        log.error(ioe)
+    except UnicodeError as ue:
+        log.error(ue)
+    except Exception as e:
+        log.error(e)
+
+    log.info(("Finalizado o processo de envio de email's."
+              " Enviado um total de {0} emails").format(total_email_enviados))
 if __name__ == "__main__":
     main()
